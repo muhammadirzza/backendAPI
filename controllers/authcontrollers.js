6e1bd81bfbf9b99144a6bda8862a22f1be6d6abb
muhammadirzza <muhammadirzza@gmail.com>
2020-05-11 14:01:26 +0700

    userverified

--- controllers/authcontrollers.js
+++ controllers/authcontrollers.js
@@ -12,32 +12,34 @@ module.exports={
             if(err) return res.status(500).send(err)    //kalau sqlnya error maka send err
             if(result.length) {
                 console.log('username sudah dipakai')
-                if(err) return res.status(500).send({message : "username sudah dipakai"})    //kalau usernamenya sudah ada maka harus pakai yang lain untuk usernamenya              
+                return res.status(200).send({status : false})    //kalau usernamenya sudah ada maka harus pakai yang lain untuk usernamenya              
             }else{
                 console.log('lewat')
                 sql=`insert into users set ?`   //kalau usernamenya tidak ada maka user berhasil register, masukkan data ke dalam database
                 var data={
                     username:username,
                     password:hashpass,   //password yang di input ke database berupa password yang sudah di encrypt
-                    email                  //penulisannya sama dengan email:email
+                    email,               //penulisannya sama dengan email:email
+                    lastlogin: new Date()            
                 }
                 db.query(sql,data,(req,result2)=>{
                     if(err) return res.status(500).send(err)    //kalau sqlnya error maka send err
                     //setelah itu kirimkan link email verifikasi
-                    var linkVerification=`http://localhost:3000/verified?id=${result2.insertId}&password=${hashpass}` //link halaman frontend 'id' sama dengan yang di database
+                    const token=createJWTToken({id:result2.insertId, username:username})    //buat token untuk dikirm ke front-end
+                    var linkVerification=`http://localhost:3000/verified?token=${token}` //link halaman frontend 'id' sama dengan yang di database
                     transporter.sendMail({
                         from:'testing <irzza.pwdk@gmail.com>',
                         to:email,
                         subject:'Testing email',
                         html:`Tolong klik link berikut untuk verifikasi :
-                        <a href=${linkVerification}>Minimales Verification<a/>`,
+                        <a href=${linkVerification}> Verification<a/>`,
                     },(err, result3)=>{
                         if(err) return res.status(500).send(err)    //kalau sqlnya error maka send err
-                        sql=`select * from users where id=${result2.insertId}`
+                        sql=`select * from users where iduser=${result2.insertId}`
                         db.query(sql,(err,result4)=>{
                             if(err) return res.status(500).send(err)    //kalau sqlnya error maka send err
-                            const token=createJWTToken({id:result4[0].id, username:result4[0].username})    //buat token untuk dikirm ke front-end
-                            return res.status(200).send({...result4[0], token})     //akan menjadi res.data di front end
+                            // const token=createJWTToken({id:result4[0].id, username:result4[0].username})    //buat token untuk dikirm ke front-end
+                            return res.status(200).send({...result4[0], token, status:true})     //akan menjadi res.data di front end
                         })  
                     })
                 })
@@ -52,13 +54,58 @@ module.exports={
         var sql=`select * from users where username = '${username}' and password = '${hashpass}'`
         db.query(sql, (err,result)=>{
             if(err) return res.status(500).send(err)
-            if(result.length) {                         //jika user ada
-                const token=createJWTToken({id:result[0].id, username:result[0].username})    //buat token
-                return res.status(200).send({...result[0], token:token})    //kirim result dari database beserta token ke front-end berupa objek
+            if(result.length) {
+                console.log(result[0].iduser)                         //jika user ada
+                const token=createJWTToken({id:result[0].iduser, username:result[0].username})    //buat token
+                return res.status(200).send({...result[0], token:token, status:true})    //kirim result dari database beserta token ke front-end berupa objek
             }else{
-                return res.status(500).send({message : "user tidak ditemukan"})
+                return res.status(200).send({status : false})
             }
         })
 
     },
+
+    userverified:(req, res) => {
+        console.log(req.user, 'ini req.user')
+        const {id} = req.user
+        var obj={
+            isverified:1
+        }
+        var sql = `update users set ? where iduser=${id}`
+        db.query(sql, obj,(err,res1)=>{
+            if(err) return res.status(500).send(err)
+            sql=`select * from users where iduser=${id}`
+            db.query(sql,(err,res2)=>{
+                if(err) return res.status(500).send(err)
+                return res.status(200).send(res2[0])
+            })  
+        })
+    },
+
+    sendmailverified:(req, res) => {
+        const {iduser, username} = req.body
+        const token=createJWTToken({id:iduser, username:username})    //buat token untuk dikirm ke front-end
+        var linkVerification=`http://localhost:3000/verified?${token}` //link halaman frontend 'id' sama dengan yang di database
+        transporter.sendMail({
+            from:'testing <irzza.pwdk@gmail.com>',
+            to:email,
+            subject:'Testing email',
+            html:`Tolong klik link berikut untuk verifikasi :
+            <a href=${linkVerification}> Verification<a/>`,
+        },(err, result3)=>{
+            if(err) return res.status(500).send(err)    //kalau sqlnya error maka send err
+            return res.status(200).send({status:true})
+        })
+    },
+
+    keeplogin:(req,res) => {
+        console.log(req.user)
+        var sql = `select * from users where iduser=${req.user.id}`
+        db.query(sql, (err,res1)=>{
+            if(err) return res.status(500).send(err)
+            // console.log(res1, 'ini res1')
+            const token=createJWTToken({id:res1[0].iduser, username:res1[0].username})    //buat token
+            return res.status(200).send({...res1[0], token:token})
+        })
+    }
 }
--- routers/authrouter.js
+++ routers/authrouter.js
@@ -4,10 +4,12 @@ const router=express.Router()
 const {auth}=require('../helper/jwtauth')
 
 router.post('/register',Authcontrollers.userregister)
+router.get('/verified',auth,Authcontrollers.userverified)
+router.post('/sendmailverification',Authcontrollers.sendmailverified)
+router.get('/login', Authcontrollers.userlogin) 
+router.get('/keeplogin', auth, Authcontrollers.keeplogin) //sudah pakai token sehingga tidak butuh params lagi, pakai req.user
 // router.get('/keeplogin/:iduser', Usercontrollers.keeplogin) //paramsnya sama dengan di user controller
-// router.get('/keeplogin', auth, Usercontrollers.keeplogin) //sudah pakai token sehingga tidak butuh params lagi, pakai req.user
 // router.put('/verified', Usercontrollers.userverified) 
-// router.get('/login', Usercontrollers.userlogin) 
 // router.post('/fblogin', Usercontrollers.loginfacebook) 
 
 module.exports=router
