298c12a386e7cc0c0d2f7dcc1bc7faf548274c52
muhammadirzza <muhammadirzza@gmail.com>
2020-05-11 15:25:55 +0700

    backend

--- connections/mysqldb.js
+++ connections/mysqldb.js
@@ -1,11 +1,7 @@
 const mysql=require('mysql')
 
 const db=mysql.createConnection({
-    host:'localhost',
-    user:'Irzza',
-    password:'09091993Mi!!',//dari workbench
-    database:'team_api',
-    port:'3306'
+
 })
 
 db.connect((err)=>{         //cek apakah sudah terubung ke mysql
